cc1b7e636e414d6f561039c464cd96942084ea0e
TangyOrigami <csaenz613@gmail.com>
2024-01-27 13:00:07 -0600

    adding boiler plate for form

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"html/template"
 	"net/http"
 	"path/filepath"
+
+    "github.com/bmizerany/pat"
 )
 
 type PageVariables struct {
@@ -16,6 +18,12 @@ func main() {
 	http.HandleFunc("/service1", ServicePage)
 	http.HandleFunc("/service2", ServicePage)
 	http.HandleFunc("/service3", ServicePage)
+
+    mux := pat.New()
+
+    mux.Get("/", http.HandlerFunc(Form))
+    mux.Post("/", http.HandlerFunc(Send))
+    mux.Get("/confirmation", http.HandlerFunc(Confirmation))
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
@@ -41,6 +49,29 @@ func ServicePage(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "service.html", pageVariables)
 }
 
+func Form(w http.ResponseWriter, r *http.Request) {
+	pageVariables := PageVariables{
+		Title: "Sample Site",
+	}
+
+	renderTemplate(w, "form.html", pageVariables)
+}
+
+func Confirmation(w http.ResponseWriter, r *http.Request) {
+	pageVariables := PageVariables{
+		Title: "Sample Site",
+	}
+
+    renderTemplate(w, "confirmation.html", pageVariables)
+}
+
+func Send(w http.ResponseWriter, r *http.Request) {
+    // Step 1: Validate Form
+    // Step 2: Send message in email
+    // Step 3: Redirect to confirmation page
+}
+
+
 func renderTemplate(w http.ResponseWriter, templateName string, data interface{}) {
 	tmpl, err := template.New(templateName).ParseFiles(
 		filepath.Join("templates", templateName),
@@ -55,4 +86,3 @@ func renderTemplate(w http.ResponseWriter, templateName string, data interface{}
 
 	tmpl.Execute(w, data)
 }
-
